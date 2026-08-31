b214ed5f930bee9d9057134fa4b0c549a6540c58
Alexander Alexandrov <git@alexo.io>
2026-09-01 01:20:07 +0300

    Build Dioxus CLI from source on Vercel

--- vercel-install.sh
+++ vercel-install.sh
@@ -5,26 +5,15 @@ export RUSTUP_HOME="/rust"
 export CARGO_HOME="$HOME/.cargo"
 export PATH="/rust/bin:$CARGO_HOME/bin:$PATH"
 
-# Needed by prerender.sh
 dnf install -y lsof
-
-# Vercel already provides Rust/rustup
-rustc --version
-cargo --version
-rustup --version
 
 rustup target add wasm32-unknown-unknown
 
-# Install cargo-binstall into /vercel/.cargo/bin
-curl -L --proto '=https' --tlsv1.2 -sSf \
-  https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
-  | bash
-
-# Install the Dioxus version your project uses
-cargo binstall \
-  --no-confirm \
-  --force \
+# Build dx against Vercel's glibc instead of downloading incompatible binary
+cargo install \
+  --locked \
+  --version 0.7.3 \
   --root "$CARGO_HOME" \
-  dioxus-cli@0.7.3
+  dioxus-cli
 
 dx --version
